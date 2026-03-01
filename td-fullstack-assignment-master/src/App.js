810b7f099566ba78b1c5647daec20e98077077de
mgorabbani <mgorabbani@gmail.com>
2026-03-01 16:06:17 +0100

    feat: implement detectSums function

--- td-fullstack-assignment-master/src/App.js
+++ td-fullstack-assignment-master/src/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import { calculateResult } from './utils';
-import './App.css';
+import React, { Component } from "react";
+import { calculateResult } from "./utils";
+import "./App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: '',
-      userInput: '',
-      result: '',
-      error: 'Write something',
+      value: "",
+      userInput: "",
+      result: "",
+      error: "Write something",
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -32,17 +32,25 @@ class App extends Component {
     return (
       <div className="App">
         <form className="App-form" onSubmit={this.handleSubmit}>
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-          { !error &&
-            <p>
-              <span>Result for input '{userInput}' is '{result}'</span>
-            </p>
-          }
-          { error &&
-            <p className="App-error">
-              {error}
-            </p>
-          }
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+          {!error && (
+            <>
+              <p>Result for input '{userInput}' is</p>
+              <ol>
+                {result.map((res, index) => (
+                  <li key={index}>
+                    {userInput[res.pA]} + {userInput[res.pB]} ={" "}
+                    {userInput[res.sum]}
+                  </li>
+                ))}
+              </ol>
+            </>
+          )}
+          {error && <p className="App-error">{error}</p>}
         </form>
       </div>
     );
--- td-fullstack-assignment-master/src/utils.js
+++ td-fullstack-assignment-master/src/utils.js
@@ -1,15 +1,28 @@
-export const detectSums = (array) => {
-  return null;
+export const detectSums = (array = []) => {
+  const results = [];
+
+  for (let pA = 0; pA < array.length; pA++) {
+    for (let pB = pA + 1; pB < array.length; pB++) {
+      const target = array[pA] + array[pB];
+      for (let sum = 0; sum < array.length; sum++) {
+        if (sum === pA || sum === pB) continue;
+        if (target === array[sum]) {
+          results.push({ pA, pB, sum });
+        }
+      }
+    }
+  }
+  return results;
 };
 
 export function calculateResult(input) {
-  const parsedInput = input.split(',').map(i => parseInt(i.trim(), 10));
+  const parsedInput = input.split(",").map((i) => parseInt(i.trim(), 10));
   let error = null;
-  let result = '';
+  let result = "";
   try {
-    result = detectSums(input);
+    result = detectSums(parsedInput);
   } catch (e) {
     error = e.message;
   }
-  return { input: parsedInput, result, error }
+  return { input: parsedInput, result, error };
 }
